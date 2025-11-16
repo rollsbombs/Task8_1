ed1e4a294a7b2591995d9f05816d62f76f883ad4
Eleonora Kuranova <aflyalf@gmail.com>
2025-11-16 20:46:20 +0300

    исправила, не должен бвыть пустой

--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -1,4 +1,8 @@
 package com.company;
 
-public class Student {
+public class Student{
+    public void learn() {
+        System.out.println("Я учусь. .zZ");
+    System.out.println("Я закончил учиться");
+    }
 }
--- src/com/company/StudentWithClock.java
+++ src/com/company/StudentWithClock.java
@@ -1,4 +1,15 @@
 package com.company;
 
-public class StudentWithClock {
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class StudentWithClock extends Student {
+    public void learn() {
+        System.out.println("Я учусь. .zZ)");
+        LocalTime currentTime = LocalTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        System.out.println("Текущее время: " + currentTime.format(formatter));
+        System.out.println("Я закончил учиться");
+    }
+
 }
